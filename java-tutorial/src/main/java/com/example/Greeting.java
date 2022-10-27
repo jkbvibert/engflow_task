e509d4e04af493d80c09958650f847b340773745
jkbvibert <116781580+jkbvibert@users.noreply.github.com>
2022-10-26 23:32:13 -0500

    Add files via upload

---
+++ java-tutorial/src/main/java/com/example/Greeting.java
@@ -0,0 +1,15 @@
+package com.example;
+
+//import com.engflow.fancytext.FancyFormatter;
+
+/*public class FancyFormatter {
+	public static void sayHiFancy() {
+		System.out.println("Why, hello there. As you can see, I'm now using the Fancy Text library.");
+	}
+}*/
+
+public class Greeting {
+    public static void sayHi() {
+        System.out.println("Hi!");
+    }
+}
